2d3df3ef839e311cd162a6dfa09cfb263cb7dfc1
ewriq <102306313+ewriq@users.noreply.github.com>
2025-07-23 23:38:40 +0300

    Update +page.svelte

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -24,36 +24,39 @@
   {/if}
 </svelte:head>
 
+
 <div class="bg-[#FDF6E9] min-h-screen flex flex-col font-sans">
-  <main class="flex grow mt-[56px] py-14 w-full">
-    <div class="container mx-auto px-4">
-      <article
-        class="w-[1400px] mx-auto bg-white/50 p-6 sm:p-10 rounded-2xl shadow-lg"
+
+  <main class="flex-grow w-full px-4 py-10 sm:py-14">
+
+    <article
+      class="max-w-6xl mx-auto bg-white/50 p-6 sm:p-8 md:p-10 rounded-2xl shadow-lg"
+    >
+      {#if meta.imageUrl}
+   
+        <img
+          src={meta.imageUrl}
+          alt="{meta.title} için kapak resmi"
+          class="w-full h-48 sm:h-64 md:h-80 object-cover rounded-xl mb-6 md:mb-8 shadow-md"
+        />
+      {/if}
+
+ 
+      <h1 class="text-3xl sm:text-4xl lg:text-5xl font-extrabold text-gray-900 mb-4">
+        {meta.title}
+      </h1>
+
+    
+      <p class="text-base sm:text-lg text-gray-600 mb-8 border-l-4 border-[#6C757D] pl-4">
+        {meta.description}
+      </p>
+
+      <hr class="my-6 md:my-8" />
+      <div
+        class="prose prose-lg max-w-none prose-p:text-gray-700 prose-h2:text-gray-800 prose-a:text-blue-600 hover:prose-a:text-blue-800"
       >
-        {#if meta.imageUrl}
-          <img
-            src={meta.imageUrl}
-            alt="{meta.title} için kapak resmi"
-            class="w-full h-64 object-cover rounded-xl mb-8 shadow-md"
-          />
-        {/if}
-
-        <h1 class="text-3xl sm:text-5xl font-extrabold text-gray-900 mb-4">
-          {meta.title}
-        </h1>
-
-        <p class="text-lg text-gray-600 mb-8 border-l-4 border-[#6C757D] pl-4">
-          {meta.description}
-        </p>
-
-        <hr class="my-8" />
-
-        <div
-          class="prose prose-lg max-w-none prose-h2:text-gray-800 prose-a:text-blue-600 hover:prose-a:text-blue-800"
-        >
-          <svelte:component this={data.content} />
-        </div>
-      </article>
-    </div>
+        <svelte:component this={data.content} />
+      </div>
+    </article>
   </main>
 </div>
