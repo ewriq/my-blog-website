9176f653c6e814de1ac1b84e64aeb191f43397dc
ewriq <102306313+ewriq@users.noreply.github.com>
2025-07-24 12:55:07 +0300

    add sitemap.xml

--- svelte.config.js
+++ svelte.config.js
@@ -5,19 +5,17 @@ import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// .svelte dosyalarına ek olarak .md dosyalarını da tanımasını sağlıyoruz.
 	extensions: ['.svelte', '.md'],
 
 	preprocess: [
 		vitePreprocess(),
-		// mdsvex'i .md uzantısı için etkinleştiriyoruz.
-		mdsvex({
+			mdsvex({
 			extensions: ['.md']
 		})
 	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
 	}
 };
 
